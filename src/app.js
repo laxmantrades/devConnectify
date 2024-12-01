69511e9fb99ab45b3d3d327ce9f79d5a72be996b
Laxman Giri <laxmanvatij@gmail.com>
2024-12-01 17:57:56 +0100

    get && post && delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/dashbaord", (req, res) => {
-  res.send("This is a dashboard!");
+//--The order is very important--//
+
+app.get("/user",(req,res)=>{
+    res.send("Succesfully got the user data")
+})
+
+app.post("/user",(req,res)=>{
+    //Db logic here here and then
+    res.send("Succefully send data to Db")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Succesfully deleted the data")
+})
+
+app.use("/user", (req, res) => {
+  res.send("This is a user profile");
 });
-app.use("/hello", (req, res) => {
-  res.send("This is a hello!");
-});
-app.use("/path", (req, res) => {
-  res.send("Hello from the path");
-});
+
+
 app.listen(7777, () => {
   console.log("Server started succesfully");
 });
